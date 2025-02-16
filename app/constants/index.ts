c466a8139a934018dcec1a7fcdf39c82372b51c2
NguyenKhanh0209 <nguyentranquockhanh294@gmail.com>
2025-02-16 12:11:43 +0700

    footer

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -17,22 +17,3 @@ export const FOOTER_LINKS = [
     links: ["Contact us"],
   },
 ];
-
-export const FOOTER_CONTACT_INFO = {
-  title: "Contact Us",
-  links: [
-    { label: "Admin Officer", value: "123-456-7890" },
-    { label: "Email Officer", value: "hilink@akinthil.com" },
-  ],
-};
-
-export const SOCIALS = {
-  title: "Social",
-  links: [
-    "/facebook.svg",
-    "/instagram.svg",
-    "/twitter.svg",
-    "/youtube.svg",
-    "/wordpress.svg",
-  ],
-};
