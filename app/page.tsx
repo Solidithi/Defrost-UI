dfbe9b480e3a54122bcb57db1ed12cc5d2712ac2
Long Phan <phannguyenhoanglong@gmail.com>
2025-02-16 17:50:39 +0700

    feat: (homePage): add Our Team component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import HeroSection from './components/Homepage_Sections/heroSection'
 import KeyFeatureSection from './components/Homepage_Sections/keyFeatureSection'
+import OurTeam from './components/Homepage_Sections/ourTeam'
 
 export default function Home() {
   return (
     <div className="w-full">
       <HeroSection />
       <KeyFeatureSection />
+      <OurTeam />
       {/* <FeaturesSection />
       <CTASection /> */}
 
