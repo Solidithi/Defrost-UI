e7890b4026c8159bee59d94e0f4d4c4ce76830de
ducmint864 <nddminh2021@gmail.com>
2025-03-09 21:20:15 +0700

    refactor: merge with remote branch

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,16 @@
 import Image from 'next/image'
-import HeroSection from './components/Homepage_Sections/heroSection'
-import KeyFeatureSection from './components/Homepage_Sections/keyFeatureSection'
-import PartnerSection from './components/Homepage_Sections/partnerSection'
-import OurTeamSection from './components/Homepage_Sections/ourTeamSection'
+import HeroSection from './components/homepage-sections/HeroSection'
+import KeyFeatureSection from './components/homepage-sections/KeyFeatureSection'
+import OurTeam from './components/homepage-sections/OurTeam'
 
 export default function Home() {
-  return (
-    <div className="w-full">
-      <HeroSection />
-      <KeyFeatureSection />
-      {/* <PartnerSection/> */}
-      <OurTeamSection />
-      {/* <FeaturesSection />
+	return (
+		<div className="w-full">
+			<HeroSection />
+			<KeyFeatureSection />
+			<OurTeam />
+			{/* <FeaturesSection />
       <CTASection /> */}
-
-
-    </div>
-  )
+		</div>
+	)
 }
