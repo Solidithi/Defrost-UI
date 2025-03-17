f9cc9c54244dff88552eb5f3c31292b9cd030676
NguyenKhanh0209 <nguyentranquockhanh294@gmail.com>
2025-03-17 14:09:13 +0700

    refactor: Delete KeyFeatureSection and ourTeamSection

--- app/components/homepage-sections/KeyFeatureSection.tsx
+++
@@ -1,9 +0,0 @@
-const KeyFeatureSection = () => {
-	return (
-		<section className="py-20 px-10 bg-gray-100">
-			<h2 className="text-4xl font-bold text-center">Hi</h2>
-			<p className="text-center mt-4 text-lg">Nào nào nào nào</p>
-		</section>
-	)
-}
-export default KeyFeatureSection
