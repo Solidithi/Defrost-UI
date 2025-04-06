d52f03499b5a14bfb240edc6cbb9a0c7fb6bc634
notlongfen <phannguyenhoanglong@gmail.com>
2025-04-06 09:07:55 +0700

    feat(project detail): add missing page init

---
+++ app/project-detail/[id]/page.tsx
@@ -0,0 +1,11 @@
+import ProjectDetail from './project-detail'
+
+const ProjectDetailPage = () => {
+	return (
+		<div className="">
+			<ProjectDetail />
+		</div>
+	)
+}
+
+export default ProjectDetailPage
