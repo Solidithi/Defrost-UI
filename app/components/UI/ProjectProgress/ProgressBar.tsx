f085388b1d0c31036102dbaab6737bce4424ad8f
ducmint864 <nddminh2021@gmail.com>
2025-04-22 00:34:05 +0700

    feat(ProgressBar): add custom bar and color class to progress bar comp

--- app/components/UI/ProjectProgress/ProgressBar.tsx
+++ app/components/UI/ProjectProgress/ProgressBar.tsx
@@ -5,8 +5,18 @@ interface ProgressBarProps {
 	index: number
 	total: number
 	duration?: number
+	overrideClassName?: boolean
+	barClassName?: string
+	colorClassName?: string
 }
-const ProgressBar = ({ index, total, duration = 2000 }: ProgressBarProps) => {
+const ProgressBar = ({
+	index,
+	total,
+	duration = 2000,
+	overrideClassName = false,
+	barClassName,
+	colorClassName,
+}: ProgressBarProps) => {
 	const [progress, setProgress] = useState(0)
 	const [nowProgress, setNowProgress] = useState(0)
 
@@ -27,12 +37,20 @@ const ProgressBar = ({ index, total, duration = 2000 }: ProgressBarProps) => {
 	return (
 		<div className="flex justify-center">
 			<div
-				className="w-1/2 m-6
-       glass-component-1 rounded-lg h-1 relative overflow-hidden"
+				className={
+					overrideClassName && barClassName
+						? barClassName
+						: `w-1/2 m-6
+       glass-component-1 rounded-lg h-1 relative overflow-hidden w-full ${barClassName}`
+				}
 			>
 				<div
-					className="bg-gradient-to-r from-[#F05550] to-[#54A4F2]
-           h-full transition-all ease-out duration-700"
+					className={
+						overrideClassName && colorClassName
+							? colorClassName
+							: `bg-gradient-to-r from-[#F05550] to-[#54A4F2]
+           h-full transition-all ease-out duration-700 ${colorClassName}`
+					}
 					style={{ width: `${progress}%` }}
 				/>
 			</div>
