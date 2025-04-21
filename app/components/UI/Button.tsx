932a8b1014f370042c4cf4f47526b747c5eb4bbf
ducmint864 <nddminh2021@gmail.com>
2025-04-21 16:01:50 +0700

    feat: add `disabled` prop to Button comp

--- app/components/UI/Button.tsx
+++ app/components/UI/Button.tsx
@@ -4,19 +4,27 @@ interface ButtonProps {
 	children: React.ReactNode
 	onClick?: () => void
 	className?: string
+	disabled?: boolean
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick, className }) => {
+const Button: React.FC<ButtonProps> = ({
+	children,
+	onClick,
+	className,
+	disabled = false,
+}) => {
 	return (
 		<button
 			onClick={onClick}
-			className={`
-            px-4 py-2 warm-cool-bg text-white rounded-full font-comfortaa 
-    transition-all duration-300 ease-in-out 
-    hover:opacity-80 hover:shadow-lg hover:scale-105 
-    active:scale-95 active:opacity-90 
-    ${className}
-        `}
+			className={
+				disabled
+					? 'cursor-not-allowed rounded-full font-comfortaa bg-white/10 opacity-50 text-white px-4 py-2'
+					: `px-4 py-2 warm-cool-bg text-white rounded-full font-comfortaa transition-all duration-300 ease-in-out 
+					hover:opacity-80 hover:shadow-lg hover:scale-105 
+					active:scale-95 active:opacity-90 ${className}
+        `
+			}
+			disabled={disabled}
 		>
 			{children}
 		</button>
