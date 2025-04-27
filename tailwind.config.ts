a5da0f6708b54bd31177ae8a15462b817b6b86f8
ducmint864 <nddminh2021@gmail.com>
2025-04-28 01:29:34 +0700

    refactor(tailwind): comment out broken border config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,11 +51,11 @@ const config: Config = {
 			animation: {
 				shine: "shine 5s linear infinite",
 			},
-			borderRadius: {
-				lg: "var(--radius)",
-				md: "calc(var(--radius) - 2px)",
-				sm: "calc(var(--radius) - 4px)",
-			},
+			// borderRadius: {
+			// 	lg: "var(--radius)",
+			// 	md: "calc(var(--radius) - 2px)",
+			// 	sm: "calc(var(--radius) - 4px)",
+			// },
 			// colors: {
 			// 	background: "hsl(var(--background))",
 			// 	foreground: "hsl(var(--foreground))",
