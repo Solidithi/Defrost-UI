e2844d9079197886d41b641d837641f325ee81f8
ducmint864 <nddminh2021@gmail.com>
2025-05-13 18:59:27 +0700

    feat(my-staking): implement GET endpoint for fetching user launchpool stakes

---
+++ app/api/my-staking/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest } from "next/server";
+import { prismaClient } from "@/app/lib/prisma";
+import { normalizeAddress } from "@/app/utils/address";
+import { stringify } from "superjson";
+import "@/app/lib/superjson-init";
+
+export async function GET(req: NextRequest) {
+	const searchParams = req.nextUrl.searchParams;
+	const userID = searchParams.get("user-id");
+	const chainID = Number(searchParams.get("chain-id"));
+
+	if (!userID || !chainID) {
+		return Response.json(
+			{ message: "Missing user ID or chain ID" },
+			{ status: 400 }
+		);
+	}
+
+	if (Number.isNaN(chainID)) {
+		return Response.json({ message: "Invalid chain ID" }, { status: 400 });
+	}
+
+	try {
+		const launchpoolRows = await prismaClient.launchpool_stake.findMany({
+			where: {
+				user_id: normalizeAddress(userID),
+			},
+			select: {
+				launchpool_id: true,
+				launchpool: {
+					where: {
+						chain_id: chainID,
+					},
+				},
+			},
+		});
+		const launchpools = launchpoolRows.map((row) => row.launchpool);
+		const responseData = {
+			launchpools,
+		};
+
+		return Response.json(stringify(responseData));
+	} catch (e) {
+		console.error("Error fetching launchpool stake:", e);
+		return Response.json(
+			{ message: "Error fetching user stake" },
+			{ status: 500 }
+		);
+	}
+}
