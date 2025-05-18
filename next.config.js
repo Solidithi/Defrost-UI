6eeba74b6c3e5505866703dbf72cbc46d1f82228
NguyenKhanh0209 <nguyentranquockhanh294@gmail.com>
2025-05-18 15:56:13 +0700

    chore(nextconfig): update import image path

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: [
-			"images.unsplash.com",
-			"www.facebook.com",
-			"scontent.fsgn5-13.fna.fbcdn.net", // Add the Facebook CDN domain
-			"i.seadn.io", // Add the Seadn CDN domain
-			"www.google.com", // Add the Google CDN domain
-			"i.pinimg.com", // Add the Pinterest CDN domain
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**",
+			},
+			{
+				protocol: "http",
+				hostname: "**",
+			},
 		],
 	},
 };
