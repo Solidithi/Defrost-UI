58a4f4f6ea6a404fc87902bf023efbf359cf8b1f
mailinh <nddminh2021@gmail.com>
2025-07-14 16:10:14 +0700

    feat(ConnectButton): replace appkit-button with custom-style button and integrate our appkit hook

--- app/components/UI/button/ConnectButton.tsx
+++ app/components/UI/button/ConnectButton.tsx
@@ -1,7 +1,14 @@
+import { useAppKit } from '@/app/hooks/useAppKit'
+
 export default function ConnectButton() {
+	const { openWalletModal } = useAppKit()
+
 	return (
-		<div>
-			<appkit-button />
-		</div>
+		<button
+			className="rounded-full purple-blue-gradient text-white px-6 py-3 font-semibold focus:outline-none focus:ring-[#6D64F7]"
+			onClick={() => openWalletModal()}
+		>
+			Connect Wallet
+		</button>
 	)
 }
